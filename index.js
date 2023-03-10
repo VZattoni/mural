cd3b0f1cc617d50b70f6033b14775d9854a08383
Victor Zattoni <97476855+VZattoni@users.noreply.github.com>
2023-03-09 21:42:38 -0300

    setting post request

--- index.js
+++ index.js
@@ -6,17 +6,7 @@ const app = express();
 
 let posts = [
     {
-        id : 'TEST123',
-        title: 'Teste do Mural',
-        description : 'Desc test'
-    },
-    {
-        id : 'TEST123',
-        title: 'Teste do Mural',
-        description : 'Desc test'
-    },
-    {
-        id : 'TEST123',
+        id : 'BIEL',
         title: 'Teste do Mural',
         description : 'Desc test'
     }
@@ -28,7 +18,15 @@ app.get("/all", (req, res)=>{
 
 });
 
-app.post("/new", (req, res)=>{
+app.post("/new", bodyParser.json(), (req, res)=>{
+    
+    let id = generateID()
+    let title = req.body.title;
+    let description = req.body.description;
+
+    posts.push({id, title, description})
+
+    res.send("Post adicionado!")
 
 });
 
@@ -37,3 +35,8 @@ app.post("/new", (req, res)=>{
 app.listen(PORT, ()=>{
     console.log('Server running on port', PORT)
 })
+
+
+function generateID(){
+    return Math.random().toString(36).substring(2,9)
+}
