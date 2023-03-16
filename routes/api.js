1cc46a4f8e67624392ceb02db60dbb94b2278b20
Victor Zattoni <97476855+VZattoni@users.noreply.github.com>
2023-03-16 17:01:12 -0300

    mobile css

--- routes/api.js
+++ routes/api.js
@@ -2,7 +2,13 @@ const express = require('express');
 const router = express.Router();
 const bodyParser = require('body-parser');
 const posts = require('../model/posts');
+const cors = require('cors');
 
+const options = {
+    origin: "http://localhost:3000" 
+}
+
+router.use(cors(options));
 
 router.get("/all", (req, res)=>{
 
